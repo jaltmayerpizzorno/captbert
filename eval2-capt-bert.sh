77ed596a8140449dfffd3fff225b56139c364123
Juan Altmayer Pizzorno <juan@altmayer.com>
2022-12-05 13:51:20 -0500

    - added shell scripts for re-evaluating the systems after adding   paired t-test significance testing;
    - added logs for re-evaluation on validation set;

---
+++ eval2-capt-bert.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+#SBATCH -c 2            # Number of Cores per Task
+#SBATCH --mem=32G       # Requested Memory
+#SBATCH -p gpu-long     # Partition
+#SBATCH -G 1            # Number of GPUs
+#SBATCH -t 50:00:00     # Job time limit
+#SBATCH -o eval2-capt-bert-%j.out # %j = job ID
+
+#T=val2014
+T=test2014
+
+python -u train_retriever.py \
+    --output_dir=`pwd`/../eval-capt-bert-$T\
+    --gen_passage_rep=False \
+    --do_train=False\
+    --do_eval=True \
+    --do_eval_pairs=False \
+    --val_data_sub_type=$T \
+    --retrieve_checkpoint=`pwd`/data/checkpoint-captbert \
+    --baseline_path=`pwd`/data/checkpoint-lxmert \
+    --per_gpu_eval_batch_size=6 \
+    --fp16=True \
+    --load_small=False \
+    --num_workers=1 \
+    --query_encoder_type=capt-bert\
+    --query_model_name_or_path="bert-base-uncased" \
+    --proj_size=768 \
+    --neg_type=other_pos+all_neg
