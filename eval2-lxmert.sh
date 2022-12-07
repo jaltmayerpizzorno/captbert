f6bf9b81d88a23a9dda07fe08fc4018f4a55aef0
Juan Altmayer Pizzorno <juan@altmayer.com>
2022-12-07 00:13:31 +0000

    - modified script to perform test evaluation on Dense-LXMERT;

--- eval2-lxmert.sh
+++ eval2-lxmert.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 #SBATCH -c 2            # Number of Cores per Task
-#SBATCH --mem=32G       # Requested Memory
+#SBATCH --mem=64G	# Requested Memory
 #SBATCH -p gpu-long     # Partition
-#SBATCH -G 1            # Number of GPUs
+#SBATCH -G 3            # Number of GPUs
 #SBATCH -t 50:00:00     # Job time limit
 #SBATCH -o eval2-lxmert-%j.out # %j = job ID
 
 #T=val2014
 T=test2014
+REPS=`pwd`/../okvqa-reps-lxmert/
 
 python -u train_retriever.py \
     --output_dir=`pwd`/../eval-lxmert-$T\
@@ -16,11 +17,12 @@ python -u train_retriever.py \
     --do_eval=True \
     --do_eval_pairs=False \
     --val_data_sub_type=$T \
+    --collection_reps_path=$REPS \
     --retrieve_checkpoint=`pwd`/data/checkpoint-lxmert\
     --per_gpu_eval_batch_size=6 \
     --fp16=True \
     --load_small=False \
-    --num_workers=1 \
+    --num_workers=3 \
     --query_encoder_type=lxmert \
     --query_model_name_or_path="unc-nlp/lxmert-base-uncased" \
     --lxmert_rep_type="{\"pooled_output\":\"none\"}" \
